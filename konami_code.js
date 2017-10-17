4917f293e1800bd11eb39260dcec6a0c60ab0cc4
Austin Kim <ask199416@gmail.com>
2017-10-17 01:44:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,21 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  var i = 0;
-
-    document.body.addEventListener('keydown', function(e) {
-      if (parseInt(e.detail || e.which) === code[i]) {
-        i++;
-        if (i === code.length) {
-          alert("Boooooom");
-          i = 0;
-        }
-      }
-      else {
-          i = 0;
-        }
-    });
-    return
+  
   // Write your JavaScript code inside the init() function
 }
 
