c3bbc674ac4b5a1872e551137f4899ab04c1b701
Austin Kim <ask199416@gmail.com>
2017-10-17 01:58:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,26 +1,27 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-//function init() {
+var index = 0;
+
+function init() {
   document.body.addEventListener('keydown', onKeyDownHandler(e){
     alert ('COOCOCOMOBER')
   })
-  // Write your JavaScript code inside the init() function
-//}
 
-var index = 0;
-
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
-  if (key === code[index]) {
-    index++;
-    if (index === code.length) {
-      //alert("Hurray!");
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        //alert("Hurray!");
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
   }
+  // Write your JavaScript code inside the init() function
 }
+
 /*
 document.body.addEventListener('keydown', function(event) {
   alert ('WHHHHAAATTTTT?!?')
