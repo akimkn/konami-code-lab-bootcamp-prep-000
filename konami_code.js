51471e2b216c637a7922306e8e4c35a5a8c9ca89
Austin Kim <ask199416@gmail.com>
2017-10-17 02:47:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,16 +3,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 var index = 0
 
 function init() {
-  document.body.addEventListener('keydown', function(e) {
-    var key = parseInt(e.detail || e.which);
-    if (key === code[index]) {
-      index++;
-      if (index === code.length - 1) {
-        alert("KONAMI CODE");
-        index = 0;
-      }
-    } else {
+  document.body.addEventListener('keydown', keyDownFunc(e) {
+}
+
+function keyDownFunc(e) {
+  var key = parseInt(e.detail || e.which);
+  if (key === code[index]) {
+    index++;
+    if (index === code.length - 1) {
+      alert("KONAMI CODE");
       index = 0;
     }
-  })
+  } else {
+    index = 0;
+  }
 }
