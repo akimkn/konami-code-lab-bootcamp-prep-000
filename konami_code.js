a48e727813ee7dc9c121b1d70565211c06435003
Austin Kim <ask199416@gmail.com>
2017-10-17 02:22:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ function onKeyDownHandler(e) {
     index = 0;
   };
 }
-document.body.addEventListener('keydown', onKeyDownHandler);
+document.body.addEventListener('keydown', onKeyDownHandler(e));
 }
 
 
