18b2e399226a4f13f361e33d1a3a3b2409d6ef82
Austin Kim <ask199416@gmail.com>
2017-10-17 01:47:36 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
   // Write your JavaScript code inside the init() function
 }
 
-let index = 0;
+var index = 0;
 
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
