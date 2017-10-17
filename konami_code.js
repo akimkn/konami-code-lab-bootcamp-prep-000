6f1b9051c3cfa27d463639b4204b5df273f515e8
Austin Kim <ask199416@gmail.com>
2017-10-17 02:48:43 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,10 +2,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 var index = 0
 
-function init() {
-  document.body.addEventListener('keydown', keyDownFunc(e))
-}
-
 function keyDownFunc(e) {
   var key = parseInt(e.detail || e.which);
   if (key === code[index]) {
@@ -17,4 +13,8 @@ function keyDownFunc(e) {
   } else {
     index = 0;
   }
+}
+
+function init() {
+  document.body.addEventListener('keydown', keyDownFunc(e))
 }
