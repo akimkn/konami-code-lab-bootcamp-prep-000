71fb716e90e384fa2461bef6b47f182f076f9065
Austin Kim <ask199416@gmail.com>
2017-10-17 01:52:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,7 +11,7 @@ function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
   if (key === code[index]) {
     index++;
-    if (index === alphabet.length) {
+    if (index === code.length) {
       alert("Hurray!");
       index = 0;
     }
